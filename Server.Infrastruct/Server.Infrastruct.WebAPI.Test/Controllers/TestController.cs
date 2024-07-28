5aacecf19d5a0d77c293713f98d5929c67df9486
ives <renhanlinbsl@163.com>
2024-07-28 20:25:14 +0800

    预留配置文件

---
+++ Server.Infrastruct/Server.Infrastruct.WebAPI.Test/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Infrastruct.WebAPI.Test.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+    }
+}
