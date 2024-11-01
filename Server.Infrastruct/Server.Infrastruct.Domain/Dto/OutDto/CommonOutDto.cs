ac7652b3439866b20ee538f2b9408b8e4ca55b05
ives <renhanlinbsl@163.com>
2024-11-01 14:58:00 +0800

    dto

---
+++ Server.Infrastruct/Server.Infrastruct.Domain/Dto/OutDto/CommonOutDto.cs
@@ -0,0 +1,27 @@
+﻿namespace Server.Infrastruct.Model.Dto.OutDto
+{
+    public class CommonOutDto
+    {
+        public string? CreateUser { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 更新人
+        /// </summary>
+        public string? UpdateUser { get; set; }
+
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+
+        /// <summary>
+        /// 通用ID字段
+        /// </summary>
+        public string? ID { get; set; }
+    }
+}
