aee18978bf74caa7fbddc25865701dfe94c39fc2
ives <renhanlinbsl@163.com>
2024-11-02 23:42:18 +0800

    swagger

--- Server.Infrastruct/Server.Infrastruct.WebAPI/CollectionExtension/SwaggerCollectionExtension.cs
+++ Server.Infrastruct/Server.Infrastruct.WebAPI/CollectionExtension/SwaggerCollectionExtension.cs
@@ -15,53 +15,31 @@ namespace Server.Infrastruct.WebAPI.CollectionExtension
         public static void AddSwagger(this IServiceCollection services)
         {
             string apiName = AppSettingsHelper.GetSetting("SwaggerSetting", "ApiName");
-            string controllerXmlName = AppSettingsHelper.GetSetting("SwaggerSetting", "ControllerXmlName");
-            string bussinessDomainXmlName = AppSettingsHelper.GetSetting("SwaggerSetting", "BussinessDomainXmlName");
-            string domainXmlName = AppSettingsHelper.GetSetting("SwaggerSetting", "DomainXmlName");
-
+           
             string swaggerXmlName = AppSettingsHelper.GetSetting("SwaggerSetting", "XmlName") ?? "";
 
             string basePath = AppContext.BaseDirectory;
-            string controllerXmlPath = Path.Combine(basePath, controllerXmlName ?? "");
-            string bussinessDomainXmlPath = Path.Combine(basePath, bussinessDomainXmlName ?? "");
-            string domainXmlPath = Path.Combine(basePath, domainXmlName ?? "");
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc(apiName, new OpenApiInfo
-                {
-                    Title = $"{apiName}接口"
-                });
+                //c.SwaggerDoc(apiName, new OpenApiInfo
+                //{
+                //    Title = $"{apiName}接口"
+                //});
 
-                if (!string.IsNullOrWhiteSpace(swaggerXmlName.Trim()))
-                {
-                    var file = swaggerXmlName.Split(',');
-                    foreach (var item in file)
-                    {
-                        string xmlPath = Path.Combine(basePath, item);
-                        if (File.Exists(xmlPath))
-                        {
-                            c.IncludeXmlComments(xmlPath, true);
-                        }
-                    }
-                }
-                #region 兼容旧的加载xml方式
-
-                if (File.Exists(controllerXmlPath))
-                {
-                    c.IncludeXmlComments(controllerXmlPath, true);
-                }
-
-                if (File.Exists(bussinessDomainXmlPath))
-                {
-                    c.IncludeXmlComments(bussinessDomainXmlPath);
-                }
-
-                if (File.Exists(domainXmlPath))
-                {
-                    c.IncludeXmlComments(domainXmlPath);
-                }
-                #endregion
+                //if (!string.IsNullOrWhiteSpace(swaggerXmlName.Trim()))
+                //{
+                //    var file = swaggerXmlName.Split(',');
+                //    foreach (var item in file)
+                //    {
+                //        string xmlPath = Path.Combine(basePath, item);
+                //        if (File.Exists(xmlPath))
+                //        {
+                //            c.IncludeXmlComments(xmlPath, true);
+                //        }
+                //    }
+                //}
+                
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Name = "Authorization",
