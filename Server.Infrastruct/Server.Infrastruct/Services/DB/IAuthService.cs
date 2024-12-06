ddc95c3a80c473fee1bd3d451374bd499262b33d
ives <ivesbao@163.com>
2024-12-06 14:09:33 +0800

    Update IAuthService.cs

--- Server.Infrastruct/Server.Infrastruct/Services/DB/IAuthService.cs
+++ Server.Infrastruct/Server.Infrastruct/Services/DB/IAuthService.cs
@@ -5,7 +5,7 @@ namespace Server.Infrastruct.Services.DB
     public interface IAuthService<T> : IAutoService<T> where T : CommonEntity, new()
     {
         new void Create(T entity,bool saveUserID=true);
-
+        new void Create(List<T> entity, bool saveUserID = true);
         new void Update(List<T> entity, bool saveUserID = true);
         new void Update(T entity, bool saveUserID = true);
     }
